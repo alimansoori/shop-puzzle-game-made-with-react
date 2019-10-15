069f4106bac7749ad2bdbd7a23eaa5a19b12d554
alimansoori <alimanssouri221@gmail.com>
2019-10-15 18:31:46 +0330

    1.0

--- 1.0/service-worker.js
+++ 1.0/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53f4513c8377e358908541f97178b04f.js"
+  "/shop-puzzle-game-made-with-react/1.0/precache-manifest.173e5243f08d0c6610e5d96d47ef31d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shop-puzzle-game-made-with-react/1.0/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
