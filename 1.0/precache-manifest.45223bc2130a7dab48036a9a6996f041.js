dc409b4b013ba3a9f1fa72d8905aabe509d5d7a5
alimansoori <alimanssouri221@gmail.com>
2019-10-15 12:04:41 +0330

    1.0

---
+++ 1.0/precache-manifest.45223bc2130a7dab48036a9a6996f041.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b420df48803872c7619c38c76448a1",
+    "url": "/shop-puzzle-game-made-with-react/1.0/index.html"
+  },
+  {
+    "revision": "882d6d0da25d1e9a7971",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/css/main.7ad63022.chunk.css"
+  },
+  {
+    "revision": "7fa3061a1e477eede677",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/js/2.90be7a9f.chunk.js"
+  },
+  {
+    "revision": "7895f891839d8c07b09e",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/js/3.124d1e59.chunk.js"
+  },
+  {
+    "revision": "2acba3bfa7cc0f05851f",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/js/4.481aadc0.chunk.js"
+  },
+  {
+    "revision": "eb9b84bcc1a7ab4f5b85",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/js/5.b4e61582.chunk.js"
+  },
+  {
+    "revision": "882d6d0da25d1e9a7971",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/js/main.974aa7fc.chunk.js"
+  },
+  {
+    "revision": "77790d9a161f78029b44",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/js/runtime~main.16c37558.js"
+  },
+  {
+    "revision": "07e121c52b58be658fb0acf23c741558",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/1.07e121c5.png"
+  },
+  {
+    "revision": "9c511df87ea8083e3ed775c36df87ccb",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/2.9c511df8.png"
+  },
+  {
+    "revision": "751ecb7ce981a85f1a5e72e168aca30c",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/3.751ecb7c.png"
+  },
+  {
+    "revision": "7ef2a68531bb64c43a21d1268a98319b",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/4.7ef2a685.png"
+  },
+  {
+    "revision": "96037b82cd18cfb7b4ce188d97d8a771",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/apple.96037b82.svg"
+  },
+  {
+    "revision": "71e1a70cb758b120009657b3841d87e5",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/ballon.71e1a70c.svg"
+  },
+  {
+    "revision": "b6f14cc52d3c98e60259a8122fc25580",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/balloon.b6f14cc5.png"
+  },
+  {
+    "revision": "60460b3f3ad4474da60b8b9bd7c8539f",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/balloons.60460b3f.svg"
+  },
+  {
+    "revision": "9a38de60b442711e9fd85bc51360fc98",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bananas.9a38de60.svg"
+  },
+  {
+    "revision": "e51e28c04f99b168c85e7ffec8800ed0",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bear.e51e28c0.svg"
+  },
+  {
+    "revision": "16aa08ebbf689edc8cdf9eef7df1da5a",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bg.16aa08eb.png"
+  },
+  {
+    "revision": "5c95bfbc0815bd3f514eb36227c242f3",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bg.5c95bfbc.png"
+  },
+  {
+    "revision": "ed1da5b0dce9d354841afb47bfcbdfd0",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bg.ed1da5b0.svg"
+  },
+  {
+    "revision": "33d1800950dbc2d547c8d80278b7ea8d",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bird1.33d18009.png"
+  },
+  {
+    "revision": "07e7e3fcfb167dc843484e3fe2681593",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bird2.07e7e3fc.png"
+  },
+  {
+    "revision": "75a8a496d7fe05bfb853ba894ff33b51",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/broccoli.75a8a496.svg"
+  },
+  {
+    "revision": "1c32752e714ca9caefd5f31b2d824e09",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/bush.1c32752e.svg"
+  },
+  {
+    "revision": "ae4da5aee02409aac649e27927cb3cd5",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/cabbage.ae4da5ae.svg"
+  },
+  {
+    "revision": "89cafa12ea2a4bbdae27d306bbf150d8",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/cake.89cafa12.svg"
+  },
+  {
+    "revision": "d77c0766224cc6c861bada4168cd94b4",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/car.d77c0766.png"
+  },
+  {
+    "revision": "7dfd44f205d28ec0f79c7bf4924f54d8",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/carrot.7dfd44f2.svg"
+  },
+  {
+    "revision": "17206da723904cf912003e5eb253ef66",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/croissant.17206da7.svg"
+  },
+  {
+    "revision": "ff80be775685c5aae48aba7bae79dca5",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/dino3.ff80be77.svg"
+  },
+  {
+    "revision": "30734eac185216390716ad43cca31cdb",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/done.30734eac.svg"
+  },
+  {
+    "revision": "705c973bc0a54cde4248c321bff96bd0",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/eggs.705c973b.svg"
+  },
+  {
+    "revision": "9b230f630f0bf27ac71883ee60d350bd",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/filter.9b230f63.svg"
+  },
+  {
+    "revision": "d42c488b5c731943767f71d365314dc8",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/flower1.d42c488b.svg"
+  },
+  {
+    "revision": "11be6a407f71610d05cf35e895aab424",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/grapes.11be6a40.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/heart.e87899ae.svg"
+  },
+  {
+    "revision": "ed55622d3cca870f9291faabb381e1d9",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/house.ed55622d.svg"
+  },
+  {
+    "revision": "39b28772c3fd086a2f5a7027dd61ce66",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/house4.39b28772.png"
+  },
+  {
+    "revision": "84db2cf4905748590a433e8fddf556d9",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/jam.84db2cf4.svg"
+  },
+  {
+    "revision": "16426bdb5f7bc5776a6df8ff63b2dff4",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/milk.16426bdb.svg"
+  },
+  {
+    "revision": "9c594fbbc2954a402815223d23161b12",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/mustard.9c594fbb.svg"
+  },
+  {
+    "revision": "fb4880daa21ea087400ee69e783f8e36",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/onion.fb4880da.svg"
+  },
+  {
+    "revision": "5a7f032e33fbff4ce480a0e086ed6fba",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/orange.5a7f032e.svg"
+  },
+  {
+    "revision": "b7c510737e2c2a186fb5bbb45efd4df0",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/pasta.b7c51073.svg"
+  },
+  {
+    "revision": "ce0b05ef979f7be2bc953f2a47c6766f",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/pineapple.ce0b05ef.svg"
+  },
+  {
+    "revision": "2938dbf24241ec348f87895fba6a4b24",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/potato.2938dbf2.svg"
+  },
+  {
+    "revision": "b486f3c6e314afae2122cf5e04bc5621",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/puzzle-game.b486f3c6.svg"
+  },
+  {
+    "revision": "956dc6e4fb30261d254d5d556ef4d87d",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/rain.956dc6e4.svg"
+  },
+  {
+    "revision": "f7955deb26d94a2da2f021ae405afdfd",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/rainbow.f7955deb.png"
+  },
+  {
+    "revision": "c4d923b981c78230e973a65b6250da47",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/shop-game.c4d923b9.svg"
+  },
+  {
+    "revision": "098fb5d3aac8e11f66d2ca5551ef107e",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/shopping-cart.098fb5d3.svg"
+  },
+  {
+    "revision": "b7d4c294ecd6041a69a8f74f1d63aea0",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/spider.b7d4c294.svg"
+  },
+  {
+    "revision": "cf33ec77a7ca266733e9a0e9e8641db7",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/stars.cf33ec77.svg"
+  },
+  {
+    "revision": "b7931bae80e691c7531de66c447bb572",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/strawberry.b7931bae.svg"
+  },
+  {
+    "revision": "eeadcd43288c92e6219f4b105e806ee5",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/sun.eeadcd43.svg"
+  },
+  {
+    "revision": "4826181a9b05a17c486fe9c57685211f",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/tomato.4826181a.svg"
+  },
+  {
+    "revision": "9f618796b8d0d29f9f4b1171fb98357c",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/tyre3.9f618796.svg"
+  },
+  {
+    "revision": "9b004dbc24d07b07f8fe89399f7811ea",
+    "url": "/shop-puzzle-game-made-with-react/1.0/static/media/watermelon.9b004dbc.svg"
+  }
+]);
